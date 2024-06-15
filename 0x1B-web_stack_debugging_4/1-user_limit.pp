0177f8a767553cde8d43f67ce70907d978d3b956
Mohamedelaroussi <midmoha93@gmail.com>
2024-06-15 23:29:07 +0000

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,2 @@
-mit file
-exec { 'Correct hard':
-  command  => 'sudo sed -i \'s/nofile 5/nofile 30000/\' /etc/security/limits.conf',
-  provider => shell,
-}
-exec { 'Correct soft':
-  command  => 'sudo sed -i \'s/nofile 4/nofile 10000/\' /etc/security/limits.conf',
-  provider => shell,
-}
+# Change the OS configuration to enable login with the holberton user and open a file without any error message.
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
